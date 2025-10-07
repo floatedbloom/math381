5708da03c895183b3c25a66e4063d17323865371
george <georgelapin23@gmail.com>
2025-10-07 13:36:09 -0700

    hmwk2 part 2

--- hmwk2/p1.py
+++ hmwk2/p1.py
@@ -4,7 +4,6 @@ def p1():
     solver = pywraplp.Solver.CreateSolver("SAT")
     #vars
     x = [solver.IntVar(0,1,"x"+str(i)) for i in range(14)]
-    
     #constraints
     #titles
     solver.Add(x[6]+x[7]+x[8]>=1)
@@ -21,10 +20,8 @@ def p1():
     solver.Add(x[7]-x[6]<=0)
     #lunch
     solver.Add(x[13]+x[11]+x[7]<=1)
-
     #objective function
     solver.Maximize(solver.Sum(x[i] for i in range(14)))
-
     #solve
     status = solver.Solve()
     if status == pywraplp.Solver.OPTIMAL:
--- hmwk2/p4.py
+++ hmwk2/p4.py
@@ -1,21 +1,34 @@
 from ortools.linear_solver import pywraplp
 
-def p2():
+def p4():
     solver = pywraplp.Solver.CreateSolver("SAT")
     #vars
+    L = [4,5,3,2,4,3,5,4]
+    B = [1,0,1,0,1,0,0,1]
+    H = [0,1,0,1,0,1,0,1]
     s1 = [solver.IntVar(0,1,"s1"+str(i+1)) for i in range(8)]
     s2 = [solver.IntVar(0,1,"s2"+str(i+1)) for i in range(8)]
     #constraints
-    
+    for i in range(8):
+        solver.Add(s1[i]+s2[i] == 1)
+    solver.Add(solver.Sum(L[i]*s1[i] for i in range(8)) <= 16) # minute duration restrictions
+    solver.Add(solver.Sum(L[i]*s1[i] for i in range(8)) >= 14)
+    solver.Add(solver.Sum(L[i]*s2[i] for i in range(8)) <= 16)
+    solver.Add(solver.Sum(L[i]*s2[i] for i in range(8)) >= 14)
+    solver.Add(solver.Sum(B[i]*s1[i] for i in range(8)) == 2) # 2 ballads per side
+    solver.Add(solver.Sum(B[i]*s2[i] for i in range(8)) == 2)
+    solver.Add(solver.Sum(H[i]*s1[i] for i in range(8)) >= 3) # 3 hits minimum on side 1
+    solver.Add(s1[4] + s1[5]>= 1) # song 5 or 6 on side 1
+    solver.Add(s1[1] + s1[3] - s2[4] <= 1) # if song 2 and 4 on side 1, song 5 on side 2
     #objective function
-    
+    solver.Maximize(0)
     #solve
     status = solver.Solve()
     if status == pywraplp.Solver.OPTIMAL:
-        print(f"Optimal Value: {solver.Objective().Value()}")
-        print(f"Optimal Solution: ")
+        print("Optimal Solutions Side 1: ", [s1[i].solution_value() for i in range(8)])
+        print("Optimal Solutions Side 2: ", [s2[i].solution_value() for i in range(8)])
     else:
         print("No optimal solution")
 
 if __name__ == '__main__':
-    p2()
+    p4()
